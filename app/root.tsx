6257dd4c716c64f04013cecc629bd48eaa176176
hogucc <uku.h1r8@gmail.com>
2024-02-04 12:22:12 +0900

    creating-contacts

--- app/root.tsx
+++ app/root.tsx
@@ -10,12 +10,16 @@ import {
   ScrollRestoration,
   useLoaderData,
 } from "@remix-run/react";
-import { getContacts } from "./data";
+import { createEmptyContact, getContacts } from "./data";
 import type { LinksFunction } from "@remix-run/node";
 import appStylesHref from "./app.css";
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: appStylesHref },
 ];
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
+};
 
 export const loader = async () => {
   const contacts = await getContacts();
